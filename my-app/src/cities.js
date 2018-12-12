9fe2c6ec6533851cd5b487c35ffcf373df07c3fd
Nouman Shahid <nouman_shahid_gr8@hotmail.com>
2018-12-12 11:41:20 +0100

    getting cities data from  backend

--- my-app/src/cities.js
+++ my-app/src/cities.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
-class Cities extends Component{
+const API = 'http://localhost:5000/cities';
+const DEFAULT_QUERY = 'redux';
+
+class Cities extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ data: data }));
+  }
   render(){
+    const { data } = this.state;
+    console.log(data);
     return(
-      <div className="App-header">
-        <h1>Cities Page</h1>
+      <div>
+        {data.map(list =>
+          <div key={list._id}>
+          <h1>{list.country}</h1>
+          <h3>{list.city}</h3>
+          </div>
+        )}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,122 +1,80 @@
 const express = require("express");
-const port = process.env.PORT || 3000;
+const Cors = require("cors");
+const port = process.env.PORT || 5000;
 const MongoClient = require('mongodb').MongoClient
 var ObjectID = require('mongodb').ObjectID;
 const bodyParser = require('body-parser')
 const app = express();
 const db = require("./key").mongoURI
 var dbase;
+
+app.use(Cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var citySchema = require('./models/City');
+
 
 mongoose.connect(db)
+
   .then(() => console.log('Mongo OK'))
   .catch(err => console.log(err));
+
+
 //Routes
 var router = express.Router();
 
-var testapi = require("./testapi")
+//post something in db
+app.post('/name/add', (req, res, next) => {
 
-app.use('./testapi', testapi);
+  var name = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  };
 
-MongoClient.connect('mongodb://nouman:punjabi1@ds047114.mlab.com:47114/mytinerary', (err, db) => {
+  dbase.collection("name").save(name, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.send('name added successfully');
+  });
+});
+// Get something from db
+// app.get('/cities', (req, res) => {
+//   citySchema.find().toArray((err, results) => {
+//     res.send(results)
+//   });
+// });
+app.get('/cities', (req, res) => {
+  citySchema.find()
+    .then(results => res.send(results))
+    .catch(err => console.log(err))
+
+});
+
+app.get('/', function(req, res) {
+  res.send('im the home page!')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('This is the contact page with a POST request')
+})
+
+app.use('/', router);
+
+MongoClient.connect('mongodb://nouman:punjabi1@ds047114.mlab.com:47114/mytinerary', { useNewUrlParser: true }, (err, db) => {
   dbase = db.db("mytinerary");
   if (err) return console.log(err)
 
 
 
-  app.listen(3000, () => {
-    console.log('app working on 3000')
+  app.listen(5000, () => {
+    console.log('app working on 5000')
   })
 })
-
-
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser')
-// const app = express();
-// const db = require("./key").mongoURI
-// app.get('/cities', (req, res) => {
-//   db.collection('cities').find().toArray((err, results) => {
-//     res.send(results)
-//   });
-// });
-//
-// const mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-//
-//
-//
-// class Database {
-//   constructor() {
-//     this._connect()
-//   }
-//
-//   _connect() {
-//     mongoose.connect(db)
-//       .then(() => {
-//         console.log('Database connection successful')
-//       })
-//       .catch(err => {
-//         console.error('Database connection error')
-//       })
-//   }
-// }
-// // module.exports = new Database()
-//
-//
-// app.get('/cities', (req, res) => {
-//   db.collection('cities').find().toArray((err, results) => {
-//     res.send(results)
-//   });
-// });
-// app.post('/name/add', (req, res, next) => {
-//
-//   var personSchema = new Schema({
-//     name: {
-//       first: String,
-//       last: String
-//     }
-//   });
-//
-//
-//   db.collection("name").save(name, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//
-//     res.send('name added successfully');
-//   });
-// });
-// // we'll create routes here
-// var router = express.Router();
-//
-// router.get('/', function(req, res) {
-//   res.send('im the home page!');
-// });
-//
-// router.get('/test', function(req, res) {
-//   res.send('Hello World!' + ' ' + 'Test Works');
-// });
-//
-// app.use('/', router);
-//
-// const MongoClient = require('mongodb').MongoClient
-// var ObjectID = require('mongodb').ObjectID;
-// MongoClient.connect('mongodb://nouman:punjabi1@ds047114.mlab.com:47114/mytinerary', (err, db) => {
-//   var dbase = db.db("crud");
-//   if (err) return console.log(err)
-//   app.listen(3000, () => {
-//     console.log('app working on 3000')
-//   })
-//
-// })
-// //Start the Server
-// //app.listen(port);
-// //console.log('Magic happens on port' + port);
